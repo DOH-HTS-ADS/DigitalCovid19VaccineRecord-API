7912af6143d22743bc401c30bab465966fba72d1
James Norris <James.Norris@dohmobile.onmicrosoft.com>
2021-10-18 11:49:58 -0700

    Continuing work on credential service job startup.cs

--- CredentialServiceJob/Startup.cs
+++ CredentialServiceJob/Startup.cs
@@ -35,17 +35,6 @@ namespace CredentialServiceJob
 
             Configuration = builder.Build();
 
-            services.AddTransient<IStartupFilter, OptionsValidationStartupFilter>();
-            services.AddMyInfrastructure(Configuration);
-            services.AddLogging(configure => configure.AddApplicationInsightsWebJobs(c => c.InstrumentationKey = Environment.GetEnvironmentVariable("APPINSIGHTS_INSTRUMENTATIONKEY"))
-            .AddConsole(configure =>
-                {
-                    // print out only 1 line per message( without this 2 are printed)
-                    configure.FormatterName = ConsoleFormatterNames.Systemd;
-                }));
-            services.AddSingleton<IQueueProcessor, Program>();
-            services.BuildServiceProvider();
-            /*
             services
                  .AddTransient<IStartupFilter, OptionsValidationStartupFilter>()
 
@@ -86,7 +75,7 @@ namespace CredentialServiceJob
                  .AddSingleton<IQueueProcessor, Program>()
                  .BuildServiceProvider();
 
-            AddSendGridClient(services);*/
+            AddSendGridClient(services);
 
             return services;
         }
