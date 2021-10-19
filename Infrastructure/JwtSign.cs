79bf0148cba7416d6b1f92f8068ec8c43c749af5
James Norris <James.Norris@dohmobile.onmicrosoft.com>
2021-10-19 15:16:40 -0700

    committ

--- Infrastructure/JwtSign.cs
+++ Infrastructure/JwtSign.cs
@@ -125,7 +125,7 @@ namespace Infrastructure
         public Thumbprint GetThumbprint(string certificate)//(string crv, string kty, string x, string y)
         {
             using var textReader = new StringReader(certificate);
-
+            throw new Exception("Made It");
             var bcCertificate = (X509Certificate)new PemReader(textReader).ReadObject();
             var publicKeyBytes = bcCertificate.CertificateStructure.SubjectPublicKeyInfo.PublicKeyData.GetBytes();
             var length = (publicKeyBytes.Length - 1) / 2;
