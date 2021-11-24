3e65d6bd3490329f62cc273f3144a8b6a280ee16
James Norris <James.Norris@dohmobile.onmicrosoft.com>
2021-11-24 08:30:36 -0800

    additional logging changes to AzureSynapseService

--- Infrastructure/AzureSynapse/AzureSynapseService.cs
+++ Infrastructure/AzureSynapse/AzureSynapseService.cs
@@ -93,7 +93,7 @@ namespace Infrastructure.AzureSynapse
                 {
                     //prepare for call to relaxed...
                     cmdVc = CreateCommand(conn, request, _azureSynapseSettings.RelaxedQuery);
-                    _logger.LogInformation($"CALLING: {_azureSynapseSettings.RelaxedQuery}; commandText={cmdVc.CommandText}; request.Id={request.Id}");
+                    _logger.LogInformation($"CALLING: {_azureSynapseSettings.RelaxedQuery}; commandText={cmdVc.CommandText}; parameters={string.Join(",", cmdVc.Parameters.Cast<SqlParameter>().ToList().Select(p => $"{p.ParameterName}={p.Value}"))}; request.Id={request.Id}");
                     rdVc = await cmdVc.ExecuteScalarAsync(cancellationToken);
                     _logger.LogInformation($"RESPONSE RECEIVED: {_azureSynapseSettings.RelaxedQuery}; rdVc={rdVc}; request.Id={request.Id}");
                     if (rdVc != null)
