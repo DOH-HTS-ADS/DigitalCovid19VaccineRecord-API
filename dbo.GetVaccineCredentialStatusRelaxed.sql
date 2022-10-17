39f0952d795dceb817ee0b6baca1c7a03a0e9426
James Allen <82835365+DOH-JRA2303@users.noreply.github.com>
2022-10-17 15:06:33 -0700

    Revert "Revert "Jra2303 sprint18""

--- dbo.GetVaccineCredentialStatusRelaxed.sql
+++ dbo.GetVaccineCredentialStatusRelaxed.sql
@@ -1,10 +1,11 @@
-﻿/****** Object:  StoredProcedure [dbo].[GetVaccineCredentialStatusRelaxed]    Script Date: 9/22/2022 2:24:57 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE OR ALTER   PROC [dbo].[GetVaccineCredentialStatusRelaxed] @FirstName [NVARCHAR](160),@LastName [NVARCHAR](160),@DateOfBirth [NVARCHAR](50),@PhoneNumber [NVARCHAR](50),@EmailAddress [NVARCHAR](256) AS
+
+ALTER   PROC [dbo].[GetVaccineCredentialStatusRelaxed] @FirstName [NVARCHAR](160),@LastName [NVARCHAR](160),@DateOfBirth [NVARCHAR](50),@PhoneNumber [NVARCHAR](50),@EmailAddress [NVARCHAR](256) AS
+
 BEGIN
 	SET NOCOUNT ON;
 	BEGIN TRY
@@ -150,10 +151,13 @@ BEGIN
                     WHEN [MatchValue] & @match7 = @match7 THEN 7
                     WHEN [MatchValue] & @match8 = @match8 THEN 8
                     WHEN [MatchValue] & @match9 = @match9 THEN 9
-                    ELSE NULL
+
+                    ELSE -1
                     END
             FROM bitmask
            WHERE [MatchValue] <> 0
+		   UNION SELECT NULL, -1
+
                )
 
           , rankedRslt
